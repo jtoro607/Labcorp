01cb52d9978f05ce515765671fa4ac22e34c7a77
jtoro607 <60904114+jtoro607@users.noreply.github.com>
2021-03-28 21:05:14 -0400

    test

---
+++ LabcorpPOMs/WebElementPOM.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabcorpPOMs
+{
+    public class WebElementPOM
+    {
+        public WebElementPOM()
+        {
+
+        }
+        public By CareerLinkLocator => By.LinkText("http://www.labcorpcareers.com/");
+        public By testLocator => By.XPath("//*[@id='block-footer']/ul/li[10]/a");
+        public By SearchFieldLocator => By.Id("search-keyword-af6e43073b");
+
+        //public IWebElement => 
+    }
+}
